73e0ca18f2e70f4afa08d7f2b695fcce04c19a19
Mihhail Verhovtsov <mihhail.verhovtsov@icefire.ee>
2012-11-21 14:40:21 +0200

    Spring WS is total bullshit! Switched to JAX-WS a did not regret. Also added logback.

--- core-ws/src/main/java/ee/ttu/ecomm/core/webservice/soap/ContractEndpoint.java
+++ core-ws/src/main/java/ee/ttu/ecomm/core/webservice/soap/ContractEndpoint.java
@@ -2,47 +2,42 @@ package ee.ttu.ecomm.core.webservice.soap;
 
 import ee.ttu.ecomm.core.domain.Contract;
 import ee.ttu.ecomm.core.service.ContractService;
-import ee.ttu.ecomm.core.webservice.soap.schema.*;
-import org.springframework.ws.server.endpoint.annotation.*;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
 import java.util.List;
 
-@Endpoint
+@Component
+@WebService(serviceName = "ContractService")
 public class ContractEndpoint {
-
-    private static final String NAMESPACE_URI = "http://ttu.ee/ecomm/schemas/messages";
 
     @Resource
     ContractService contractService;
 
-    ObjectFactory objectFactory = new ObjectFactory();
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "FindContractsRequest")
-    @ResponsePayload
-    public FindContractResponse findContracts() {
-        FindContractResponse findContractResponse = objectFactory.createFindContractResponse();
-        for (Contract contract : contractService.findContracts()) {
-            ee.ttu.ecomm.core.webservice.soap.schema.Contract c = objectFactory.createContract();
-            c.setContractNumber(contract.getContractNumber());
-            findContractResponse.getContract().add(c);
-        }
-        return findContractResponse;
+    @WebMethod
+    public List<Contract> findContracts() {
+        return contractService.findContracts();
     }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetContractRequest")
-    @Namespace(prefix = "c", uri = NAMESPACE_URI)
-    @ResponsePayload
+    @WebMethod
     public Contract getContract(long contractId) {
         return contractService.getContract(contractId);
     }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "AcceptContractRequest")
+    @WebMethod
+    public String saveContract(Contract contract) {
+        contractService.saveContract(contract);
+        return contract.getContractNumber();
+    }
+
+    @WebMethod
     public void acceptContract(long contractId) {
         contractService.acceptContract(contractId);
     }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "RejectContractRequest")
+    @WebMethod
     public void rejectContract(long contractId) {
         contractService.rejectContract(contractId);
     }
